59903ff2c3f6eaf1854e917da32ca17fdc7ef480
Mdshoaib77 <email4966@gmail.com>
2025-06-04 04:01:03 +0000

    variables learn

---
+++ basices/01_variables.js
@@ -0,0 +1,20 @@
+const account_Id = 144553
+let account_Email = "mdshoaib@gmail.com"
+var account_Password ="1234567"
+account_Country = "Bangladesh"
+let account_State;
+
+// account_Id = 2
+account_Email = "mdshowaib@gmail.com"
+account_Password="8974"
+account_Country="Afganisthan"
+
+console.log(account_Id);
+
+/*
+prefer not use var
+because of issue in block scope and functional scope
+*/
+
+console.table([account_Id,account_Email,account_Password,account_Country,account_State])
+
